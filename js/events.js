ec25559adddd683a3e120c5ecebfb24c656a7537
sam <samyakgandhiasia@gmail.com>
2023-10-24 05:20:17 +0530

    14

--- js/events.js
+++ js/events.js
@@ -2,22 +2,30 @@ var array=[
     {
         title: "STOCKS 101",
         date: "26 October 2023",
-        imgSRC:"img/event/event-4.jpg"
+        imgSRC:"img/event/event-4.jpg",
+        href:"event-4.html",
+        reg:"registration_form.html",
     },
     {
         title: "Fin-Shooting",
         date: "27 September 2023",
-        imgSRC:"img/event/event-3.jpg"
+        imgSRC:"img/event/event-3.jpg",
+        href:"event-3.html",
+        reg:"",
     },
     {
         title: "Subcommittee Interview",
         date: "2,3,4 September 2023",
-        imgSRC:"img/event/event-2.jpg"
+        imgSRC:"img/event/event-2.jpg",
+        href:"event-2.html",
+        reg:"",
     },
     {
         title: "Introductory event",
         date: "28 August 2023",
-        imgSRC:"img/event/event-1.jpg"
+        imgSRC:"img/event/event-1.jpg",
+        href:"event-1.html",
+        reg:"",
     }
     ]
     function events(id){
@@ -32,8 +40,8 @@ var array=[
                         <h4 class="text-primary">${array[i].date}</h4>
                     </div>
                     <div class="segment-overlay">
-                        <a href="" class="btn btn-primary rounded-pill py-2 px-4 m-2">More Detail <i class="fa fa-arrow-right ms-2"></i></a>
-                        <a href="registration_form.html" class="btn btn-dark rounded-pill py-2 px-4 m-2">Register for the event <i class="fa fa-arrow-right ms-2"></i></a>
+                        <a href="${array[i].href}" class="btn btn-primary rounded-pill py-2 px-4 m-2">More Detail <i class="fa fa-arrow-right ms-2"></i></a>
+                        <a href="${array[i].reg}" class="btn btn-dark rounded-pill py-2 px-4 m-2">Register for the event <i class="fa fa-arrow-right ms-2"></i></a>
                     </div>
                 </div>
             </div>`;
