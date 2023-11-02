4f465fde12f38c78451b52395908185006778fde
sam <samyakgandhiasia@gmail.com>
2023-11-02 21:30:43 +0530

    43

--- js/navbar.js
+++ js/navbar.js
@@ -11,28 +11,27 @@ load+=`<div class="container">
     </button>
     <div class="collapse navbar-collapse" id="navbarCollapse">
         <div class="navbar-nav ms-auto">
-            <a href="index.html" class="nav-item nav-link">Home</a>
-            <a href="about.html" class="nav-item nav-link">About</a>
-            <a href="teammain.html" class="nav-item nav-link">TEAM</a>
-            <a href="event.html" class="nav-item nav-link">EVENTS</a>
-            <div class="nav-item dropdown">
-                <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Pages</a>
-                <div class="dropdown-menu bg-light rounded-0 m-0">
+        <a href="index.html" class="nav-item nav-link">Home</a>
+        <a href="about.html" class="nav-item nav-link">About</a>
+        <a href="teammain.html" class="nav-item nav-link">TEAM</a>
+        <a href="event.html" class="nav-item nav-link">EVENTS</a>
+        <div class="nav-item dropdown">
+        <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Pages</a>
+        <div class="dropdown-menu bg-light rounded-0 m-0">
                     <a href="findepthmain.html" class="dropdown-item">FINDEPTH</a>
                     <a href="photo gallery.html" class="dropdown-item">IMAGE GALLERY</a>
                     <a href="blog.html" class="dropdown-item">Blog</a>
                     <a href="financial_literacy.html" class="dropdown-item">FINANCIAL LITERACY</a>
+                    <a href="https://bazaar.bullsandbearspdeu.com" class="dropdown-item" target="_blank">BAZAAR</a> 
                     </div>
                     </div>
                     <a href="contact.html" class="nav-item nav-link">Contact</a>
         </div>
         <div class="border-start ps-4 d-none d-lg-block">
-            <button type="button" class="btn btn-sm p-0"><i class="fa fa-search"></i></button>
+        <button type="button" class="btn btn-sm p-0"><i class="fa fa-search"></i></button>
         </div>
     </div>
 </nav>
 </div>`;
   document.getElementById(id).innerHTML = load;
 }
-
-    {/* <a href="https://bazaar.bullsandbearspdeu.com" class="dropdown-item" target="_blank">BAZAAR</a> */}
